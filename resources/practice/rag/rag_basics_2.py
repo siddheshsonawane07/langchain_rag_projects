e0f320ec9a68d73101627b53c11bfbb67287cd8e
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-11-06 15:13:20 +0530

    rag_basics

---
+++ resources/practice/rag/rag_basics_2.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_mistralai import MistralAIEmbeddings
+
+load_dotenv()
+# Define the persistent directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+persistent_directory = os.path.join(current_dir, "db", "chroma_db")
+
+# Define the embedding model
+embeddings = MistralAIEmbeddings(
+    model="mistral-embed")
+
+# Load the existing vector store with the embedding function
+db = Chroma(persist_directory=persistent_directory,
+            embedding_function=embeddings)
+
+# Define the user's question
+query = "Who is Odysseus' wife?"
+
+# Retrieve relevant documents based on the query
+retriever = db.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={"k": 3, "score_threshold": 0.4},
+)
+relevant_docs = retriever.invoke(query)
+
+# Display the relevant results with metadata
+print("\n--- Relevant Documents ---")
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"Document {i}:\n{doc.page_content}\n")
+    if doc.metadata:
+        print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
